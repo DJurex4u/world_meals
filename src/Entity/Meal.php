e6df3b8c9d584b08ac18d99850fa7e8c80cd60e9
Ivan Djuro <pepsiifanta@gmail.com>
2020-01-23 20:16:59 +0100

    added Meal and MealTranslation entity

---
+++ src/Entity/Meal.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MealRepository")
+ */
+class Meal implements TranslatableInterface
+{
+    use TranslatableTrait;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function changeStatus(): self
+    {
+        $this->status = !$this->status;
+
+        return $this;
+    }
+
+}
